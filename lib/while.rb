3b74da401d67e8cbea4983c48c98e779bb38c6a8
parik panchal <parikspanchal@icloud.com>
2018-12-24 11:29:33 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
-$levitation_force = 6
+$levitation_forcee = 6
 def using_while
-  while $levitation_force < 10
+  while $levitation_forcee < 10
     puts "Wingardium Leviosa"
-    $levitation_force += 1
+    $levitation_forcee += 1
   end
 end
